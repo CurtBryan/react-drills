41bcac6551f22a7f67dc9ca9ae4611a2b4684d48
Curt Bryan <kurdtbryan@gmail.com>
2019-05-07 14:02:37 -0700

    done #4

---
+++ app-4/src/login.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+export default Login extends Component {
+    constructor () {
+        super();
+
+        this.state={
+            username:"",
+            password:""
+        }
+    }
+  render() {
+    <div>
+      <input text="username" />
+      <input text="password" />
+    </div>
+  }
+}
